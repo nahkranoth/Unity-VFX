9b8daec19bc75ae968932f5393b144d5bb0c5d3b
Joey <nahkranoth3@hotmail.com>
2022-03-01 00:30:58 +0100

    Added cellshader

---
+++ Assets/Scripts/Compute/BlitPass.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class BlitRenderFeature : ScriptableRendererFeature
+{
+    class BlitRenderPass : ScriptableRenderPass
+    {
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get();
+            
+            
+        }
+    }
+    
+    BlitRenderPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        m_ScriptablePass = new BlitRenderPass();
+ 
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRendering;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
